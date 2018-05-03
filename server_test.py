45757c60c2084a5a346c669b482f97f3edca0214
Kata <katalin.molnar.kiss@gmail.com>
2018-05-03 14:36:21 +0200

    status code testing is running

--- server_test.py
+++ server_test.py
@@ -1,21 +1,27 @@
 import unittest
-from server import *
+from server import app
 from flask import Flask
 
-app = Flask(__name__)
+
+app.testing = True
 
 
 class ServerTest(unittest.TestCase):
 
     def test_status_code_200(self):
-        pass
+        # imitates a browser
+        with app.test_client() as c:
+            resp = c.get('/calculate_price/3/4')
+            self.assertEqual(200, resp.status_code)
+            #data = json.loads(resp.data)
 
     def test_status_code_400(self):
-        pass
+        with app.test_client() as c:
+            resp = c.get('/calculate_price/100/4')
+            self.assertEqual(400, resp.status_code)
 
     def test_response_is_json(self):
         pass
-
 
 if __name__ == "__main__":
     unittest.main()        
